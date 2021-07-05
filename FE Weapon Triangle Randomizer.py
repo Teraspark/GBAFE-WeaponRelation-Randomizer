ca816e4c8946c1b0d092a6ac7079fe7b12d32a06
Teraspark <33077030+Teraspark@users.noreply.github.com>
2021-07-05 14:35:48 -0400

    added missing newline in ouput

--- FE Weapon Triangle Randomizer.py
+++ FE Weapon Triangle Randomizer.py
@@ -193,7 +193,7 @@ class Window:
 		if (self.outfile.get()):
 			output = '//Weapon Relations Randomizer\n'
 			output += '//Seed: ' + self.seed.get() + '\n'
-			output += '#include "WeaponRelationDefs.event"'
+			output += '#include "WeaponRelationDefs.event"\n'
 			output += 'WeaponRelations:\n'
 			output += '\n'.join(w.output() for w in relations) + '\n'
 			Path(self.outfile.get()).write_text(output)
